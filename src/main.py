c96736ce62b399509c6a61360bb73955a4478545
helelily <hsajer@gmail.com>
2016-07-09 13:44:03 -0400

    testing directory setup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("input")
+parser.add_argument("output")
+args = parser.parse_args()
+
+with open(args.output, "w") as file_stream:
+    file_stream.write("1.00")
