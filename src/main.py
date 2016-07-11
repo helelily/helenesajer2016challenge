5f776b61608342a0a4e8e5b8241a3b95d6371f44
helelily <hsajer@gmail.com>
2016-07-11 11:34:42 -0400

    testing directory setup

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
 import argparse
-import json
-import matplotlib.pyplot as plot_tool
-import datetime
 import tranprocfunctions as function_tool
 
 parser = argparse.ArgumentParser()
@@ -9,24 +6,22 @@ parser.add_argument("input")
 parser.add_argument("output")
 arguments = parser.parse_args()
 
-plot_tool.ion()
+function_tool.plot_tool.ion()
 
-decoder = json.JSONDecoder()
 transaction_set = []
-max_time_processed = datetime.datetime.fromtimestamp(0)
+max_time_processed = function_tool.datetime.datetime.fromtimestamp(0)
 
 
 try:
     with open(arguments.input, "r") as input_stream, open(arguments.output, "w") as output_stream:
         for json_line in input_stream:
-            transaction_line = decoder.decode(json_line)
-            try:
-                transaction = function_tool.parse_transaction_line(transaction_line)
-            except ValueError:
+            transaction = function_tool.parse_transaction_line(json_line)
+            if transaction is None:
                 continue
-            max_time_processed = function_tool.update_rolling_window(transaction, max_time_processed)
-            transaction_set = function_tool.update_transaction_set(transaction_set, transaction, max_time_processed)
-            median = function_tool.plot_network_return_median(transaction_set)
-            output_stream.write(median)
+            else:
+                max_time_processed = function_tool.update_rolling_window(transaction, max_time_processed)
+                transaction_set = function_tool.update_transaction_set(transaction_set, transaction, max_time_processed)
+                median = function_tool.plot_network_return_median(transaction_set)
+                output_stream.write(median)
 except IOError:
     print "Error reading or writing to files %s, %s.  Exiting program." % (arguments.input, arguments.output)
--- src/tranprocfunctions.py
+++ src/tranprocfunctions.py
@@ -1,15 +1,37 @@
+import json
 import networkx
 import matplotlib.pyplot as plot_tool
 import datetime
 import statistics
 
 
-def parse_transaction_line(transaction_line):
-    created_time = datetime.datetime.strptime(transaction_line['created_time'], '%Y-%m-%dT%H:%M:%SZ')
-    target = transaction_line['target']
-    actor = transaction_line['actor']
-    parsed_transaction = (created_time, (target, actor))
-    return parsed_transaction
+decoder = json.JSONDecoder()
+
+
+def parse_transaction_line(json_line):
+    try:
+        transaction_line = decoder.decode(json_line)
+    except ValueError:
+        return None
+
+    try:
+        created_time = transaction_line['created_time']
+        target = transaction_line['target']
+        actor = transaction_line['actor']
+    except KeyError:
+        return None
+
+    try:
+        transaction_time = datetime.datetime.strptime(created_time, '%Y-%m-%dT%H:%M:%SZ')
+    except ValueError:
+        return None
+
+    if not transaction_time or not target or not actor:
+        return None
+
+    else:
+        parsed_transaction = (transaction_time, (target, actor))
+        return parsed_transaction
 
 
 def update_rolling_window(transaction, max_time_processed):
@@ -28,6 +50,7 @@ def plot_network_return_median(transaction_set):
     transactionGraph = networkx.Graph(pairsInWindow)
     networkx.draw_circular(transactionGraph)
     plot_tool.show()
+    plot_tool.pause(0.0001)
     plot_tool.close()
 
     degrees = [degree[1] for degree in transactionGraph.degree()]
